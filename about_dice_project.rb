eb89b0ee3ce60f1bc05de0fe81ed6c3ff862f100
Nimit <nimitagg95@gmail.com>
2018-06-21 14:36:15 +0530

    about dice project
    Signed-off-by: Nimit <nimitagg95@gmail.com>

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -2,9 +2,16 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 # Implement a DiceSet Class here:
 #
-# class DiceSet
-#   code ...
-# end
+class DiceSet
+  attr_reader :values
+  def intialize
+      @values = []
+  end
+
+  def roll(time)
+      @values = time.times.map { 1 + rand(6) }
+  end
+end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
